2eaa4939da3092adf6f4d7695400fe9b36765456
Ironboy <ironboy@users.noreply.github.com>
2023-04-10 17:52:03 +0200

    hide details on etikettprint

--- app/printEtikett.js
+++ app/printEtikett.js
@@ -7,10 +7,13 @@ function printEtikett() {
   let wDay = ['sön', 'mån', 'tis', 'ons', 'tor', 'fre', 'lör'][new Date(d).getDay()];
   wDay && (d = wDay + 'dag ' + d);
   let b = $(`<h3>Tillagad: ${d}<br><br></h3>`).insertAfter('h1');
+  let dButton = $('.showNutrionDetails:contains("Dölj")');
+  dButton.click();
   let clones = [...new Array(numberOf - 1)].map(x => $('main').clone());
   for (let clone of clones) { $('body').append(clone); }
   window.print();
   a.show();
   b.remove();
   for (let clone of clones) { clone.remove(); }
+  dButton.click();
 }
