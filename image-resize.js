27c01a60f82d8c49cc86bec4b097c4d7457e2898
Ironboy <ironboy@users.noreply.github.com>
2023-04-12 02:57:09 +0200

    gitignore scaled images

--- image-resize.js
+++ image-resize.js
@@ -6,7 +6,7 @@ const fullSizePath = path.join(__dirname, 'images', 'full-size');
 const resizedPath = path.join(__dirname, 'images', 'resized');
 
 for (let x of fs.readdirSync(fullSizePath)) {
-  for (let resizeWidth of [200, 1000]) {
+  for (let resizeWidth of [200, 1000, 1500, 2000]) {
     if (x.slice(-4) !== '.jpg') { continue; }
     let imgPath = path.join(fullSizePath, x);
     let savePath = path.join(resizedPath,
