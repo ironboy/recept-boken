c1112ea7539dd0d9c045beb5881145148b895d66
Ironboy <ironboy@users.noreply.github.com>
2023-04-15 10:08:57 +0200

    load script and style names from json

--- app/_main.js
+++ app/_main.js
@@ -1,54 +1,44 @@
 let debug = location.protocol !== 'https';
 let startTime = Date.now(), loadCounter = 0, _cl = 48;
-let ndataObj, ndata, receptMd, receptDetails, search = '';
+let ndataObj, ndata, receptMd, receptDetails, niceLog, search = '';
 
-const scripts = [
-  '*jquery', '*marked', '*pako', 'recept', 'start',
-  'menu', 'navigate', 'addEvent', 'pack', 'unpack',
-  'livsmedelslista', 'livsmedelsdetaljer', 'printEtikett',
-  'receptdetaljer', 'prepRecept', 'prepReceptDetail',
-  'calcNutrients', 'reCalculator', 'getQuantities', 'kebabify',
-  'fuzzyFraction', 'addExtraData', 'numFormatter', 'callLogger',
-  'nChart', 'nChartAnimate', 'sleep', 'fastCalc', 'loremGenerator',
-  'globalEvents'
-];
+async function _main() {
+  Array.prototype.toString = function () { return this.join(''); };
+  !debug && (console.log = () => { });
+  !debug && (console.table = () => { });
+  niceLog = (...a) => {
+    let l = a.reduce((a, c) => a + (c + '').length, a.length - 1);
+    try {
+      a[a.length - 3] && (a[a.length - 3] += ' '.repeat(_cl - l));
+    } catch (e) { }
+    return a;
+  };
 
-const styles = [
-  'fonts', 'common', 'nav', 'main', 'table', 'details',
-  'recept-in-list', 'details-images', 'charts', 'charts2-more',
-  'charts3-lines', 'print'
-];
+  console.log(...niceLog('RECEPT:  Booting up... :)', '', '© ironboy 2023'));
+  console.log(...niceLog('-'.repeat(_cl)));
 
-Array.prototype.toString = function () { return this.join(''); };
-!debug && (console.log = () => { });
-!debug && (console.table = () => { });
-const niceLog = (...a) => {
-  let l = a.reduce((a, c) => a + (c + '').length, a.length - 1);
-  try {
-    a[a.length - 3] && (a[a.length - 3] += ' '.repeat(_cl - l));
-  } catch (e) { }
-  return a;
-};
+  let { scripts, styles } = await (await
+    fetch('/json/scripts-and-styles.json')).json();
+  window.scripts = scripts;
+  const toLoad = [...scripts, ...styles.map(x => x + '.css')];
+  toLoad.map(x => {
+    let startTime = Date.now();
+    let folder = '/app';
+    x[0] === '*' && (x = x.slice(1)) && (folder = '/libs');
+    let css = x.slice(-4) === '.css';
+    folder = css ? '/css' : folder;
+    let s = document.createElement(css ? 'link' : 'script');
+    css && (s.setAttribute('rel', 'stylesheet'));
+    let src = `${folder}/${x}${css ? '' : '.js'}`;
+    s.setAttribute(css ? 'href' : 'src', src);
+    s.onload = () => {
+      console.log(...niceLog(((loadCounter + 1) + '')
+        .padStart(2, '0') + '. Loaded',
+        src, 'time', Date.now() - startTime, 'ms'));
+      ++loadCounter === toLoad.length && callLogger() && start();
+    }
+    document[css ? 'head' : 'body'].append(s);
+  });
+}
 
-console.log(...niceLog('RECEPT:  Booting up... :)', '', '© ironboy 2023'));
-console.log(...niceLog('-'.repeat(_cl)));
-
-const toLoad = [...scripts, ...styles.map(x => x + '.css')];
-toLoad.map(x => {
-  let startTime = Date.now();
-  let folder = '/app';
-  x[0] === '*' && (x = x.slice(1)) && (folder = '/libs');
-  let css = x.slice(-4) === '.css';
-  folder = css ? '/css' : folder;
-  let s = document.createElement(css ? 'link' : 'script');
-  css && (s.setAttribute('rel', 'stylesheet'));
-  let src = `${folder}/${x}${css ? '' : '.js'}`;
-  s.setAttribute(css ? 'href' : 'src', src);
-  s.onload = () => {
-    console.log(...niceLog(((loadCounter + 1) + '')
-      .padStart(2, '0') + '. Loaded',
-      src, 'time', Date.now() - startTime, 'ms'));
-    ++loadCounter === toLoad.length && callLogger() && start();
-  }
-  document[css ? 'head' : 'body'].append(s);
-});
+_main();
